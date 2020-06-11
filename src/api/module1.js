754a3a4e64464c2984593c544867e2685b782cff
pxd-fed <55471942+pxd-fed@users.noreply.github.com>
2020-06-11 10:57:09 +0900

    Update module1.js

--- src/api/module1.js
+++ src/api/module1.js
@@ -3,67 +3,57 @@ import axios from 'axios';
 const module1 = {};
 
 module1.getModule = (url, data) => {
-	try {
-		return axios({
-			method: 'get',
-			baseURL: process.env.VUE_APP_BASEURL,
-			url,
-			params: { ...data },
-		});
-	} catch (error) {
+	return axios({
+		method: 'get',
+		baseURL: process.env.VUE_APP_BASEURL,
+		url,
+		params: { ...data },
+	}).catch((error) => {
 		return console.error(`[API][module1] getModule is failed url: ${url}, error: ${error}`);
-	}
+	});
 };
 
 module1.postModule = (url, data) => {
-	try {
-		return axios({
-			method: 'post',
-			baseURL: process.env.VUE_APP_BASEURL,
-			url,
-			data,
-		});
-	} catch (error) {
+	return axios({
+		method: 'post',
+		baseURL: process.env.VUE_APP_BASEURL,
+		url,
+		data,
+	}).catch((error) => {
 		return console.error(`[API][module1] postModule is failed url: ${url}, error: ${error}`);
-	}
+	});
 };
 
 module1.putModule = (url, data) => {
-	try {
-		return axios({
-			method: 'put',
-			baseURL: process.env.VUE_APP_BASEURL,
-			url,
-			data,
-		});
-	} catch (error) {
+	return axios({
+		method: 'put',
+		baseURL: process.env.VUE_APP_BASEURL,
+		url,
+		data,
+	}).catch((error) => {
 		return console.error(`[API][module1] putModule is failed url: ${url}, error: ${error}`);
-	}
+	});
 };
 
 module1.patchModule = (url, data) => {
-	try {
-		return axios({
-			method: 'patch',
-			baseURL: process.env.VUE_APP_BASEURL,
-			url,
-			data,
-		});
-	} catch (error) {
+	return axios({
+		method: 'patch',
+		baseURL: process.env.VUE_APP_BASEURL,
+		url,
+		data,
+	}).catch((error) => {
 		return console.error(`[API][module1] patchModule is failed url: ${url}, error: ${error}`);
-	}
+	});
 };
 
 module1.deleteModule = (url) => {
-	try {
-		return axios({
-			method: 'delete',
-			baseURL: process.env.VUE_APP_BASEURL,
-			url,
-		});
-	} catch (error) {
+	return axios({
+		method: 'delete',
+		baseURL: process.env.VUE_APP_BASEURL,
+		url,
+	}).catch((error) => {
 		return console.error(`[API][module1] deleteModule is failed url: ${url}, error: ${error}`);
-	}
+	});
 };
 
 export default module1;
